4a7003da22490df4b07e4d2969acb4af31c8144f
judokaga <--->
2017-08-14 22:08:36 +0800

    columns and lines

--- myracket-mode.el
+++ myracket-mode.el
@@ -1,15 +1,38 @@
 (define-derived-mode myracket-mode nil "My Racket"
   ""
   (set (make-local-variable 'font-lock-defaults) '(myracket-font-lock-keywords))
-  (set (make-local-variable 'indent-line-function) 'myracket-indent-line)
-  (set (make-local-variable 'indent-var) 2))
+  (set (make-local-variable 'indent-line-function) 'myracket-indent-line))
+
+;; (setq lexical-binding t)
 
 (defconst myracket-font-lock-keywords
   (list "define" "car" "cdr" "cons" "list" "let" "if" "cond" "else"
 	"apply" "values"))
 
+(defun first-linep ()
+  (= 1 (count-lines 1 (point))))
+
 (defun myracket-indent-line ()
   (interactive)
-  ; (beginning-of-line)
-  (indent-line-to indent-var)
-  (setq indent-var (+ 2 indent-var)))
+  (if (first-linep)
+      (progn
+	(indent-line-to 0))
+    (progn
+      (indent-line-to
+       (column-at-pos (point))))))
+
+(defun column-at-pos (point)
+  (save-excursion
+    (goto-char point)
+    (current-column)))
+
+(defun beginning-of-form (pos)
+  (defun bof-helper (matched-right pos)
+    (if (= (char-after pos) ?\()
+	(if matched-right
+	    (bof-helper nil (1- pos))
+	  pos)
+      (bof-helper matched-right (1- pos))))
+  (if (= (char-after pos) ?\))
+      (bof-helper t pos)
+    (bof-helper nil pos)))
